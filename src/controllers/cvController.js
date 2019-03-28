f6255b74f0b084fc5d1e22791ca426599182dffb
Ankit Tiwari <ankitt255@gmail.com>
2019-03-28 20:55:41 +0530

    📝Add an admin route to get all users with CV

--- src/controllers/cvController.js
+++ src/controllers/cvController.js
@@ -37,23 +37,30 @@ let isUpdatedZipSavedInS3 = true;
  */
 router.post('/', uploadMiddleware, uploadCV);
 /**
- * @route - POST /api/cv
+ * @route - GET /api/cv
  * @desc - route for getting a direct url for CV
  * @access - private
  */
 router.get('/', getCV);
 /**
- * @route - POST /api/cv/regNo/:regNo
+ * @route - GET /api/cv/batchYear/:batchYear/regNo/:regNo
  * @desc - route for getting direct Url for CV by regNo
  * @access - private
  */
 router.get('/batchYear/:batchYear/regNo/:regNo', getCVYearAndRegNo);
 /**
- * @route - POST /api/cv/all
+ * @route - GET /api/cv/all/batchYear/:batchYear
  * @desc - route for getting all cvs as ZIP
  * @access - private
  */
 router.get('/all/batchYear/:batchYear', getAllCVsAsZip);
+
+/**
+ * @route - GET /api/cv/users
+ * @desc - route for getting all the users who have uploaded the cv
+ * @access - private
+ */
+router.get('/all/users/batchYear/:batchYear', getAllUsersWithCV);
 
 // Controllers
 async function uploadCV(req, res, next) {
@@ -178,6 +185,33 @@ async function getAllCVsAsZip(req, res, next) {
     });
 
     return res.json(url);
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ type: 'miscellaneous', message: JSON.stringify(err.message) });
+  }
+}
+
+async function getAllUsersWithCV(req, res, next) {
+  try {
+    const { user } = req;
+    const { batchYear } = req.params;
+
+    if (user.type !== userTypes.ADMIN) {
+      return res
+        .status(400)
+        .json({ message: 'You are not allowed to perform this action' });
+    }
+
+    // query the database for users with batchYear and hasUploadedCV = true
+
+    const usersWithCV = await User.find({
+      type: userTypes.USER,
+      batchYear,
+      hasUploadedCV: true,
+    }).select('name regNo -_id');
+
+    return res.json(usersWithCV);
   } catch (err) {
     return res
       .status(500)
